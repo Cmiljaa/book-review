278e475b09a9013ea5e169fe7b1b5ac0f47aef69
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-07-13 20:36:23 +0200

    Factory and seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,5 +21,26 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Book::factory(33)->create()->each(function($book){
+            $numReviews = random_int(5, 30);
+
+            //factory method expects no arguments (or a single argument to customize the factory state), and bcs of that we need to use count
+            Review::factory() -> count($numReviews) -> good() -> for($book) -> create();
+        });
+
+        Book::factory(33)->create()->each(function($book){
+            $numReviews = random_int(5, 30);
+
+            //factory method expects no arguments (or a single argument to customize the factory state), and bcs of that we need to use count
+            Review::factory() -> count($numReviews) -> avg() -> for($book) -> create();
+        });
+
+        Book::factory(34)->create()->each(function($book){
+            $numReviews = random_int(5, 30);
+
+            //factory method expects no arguments (or a single argument to customize the factory state), and bcs of that we need to use count
+            Review::factory() -> count($numReviews) -> bad() -> for($book) -> create();
+        });
     }
 }
