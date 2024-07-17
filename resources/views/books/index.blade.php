f4e1a6b3db693537077be56b29b5cd5bf84193cf
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-07-17 23:39:02 +0200

    Search by title form

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 @section('content')
 
-<form></form>
+<form action="{{route('books.index')}}" method="GET" class="mb-4 flex items-center space-x-2">
+    <input type="text" class="input h-10" name="title" placeholder="Search by title" value="{{request('title')}}">
+    <button type="submit" class="btn">Search</button>
+    <a href="{{route('books.index')}}" class="btn h-10">Clear</a>
+</form>
 
     <ul>
     @forelse ($books as $book)
