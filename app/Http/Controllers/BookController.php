215656a8ee5cab5197dda2d5f0d46874cd88601b
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-07-21 21:45:11 +0200

    Cache

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
@@ -25,7 +27,8 @@ class BookController extends Controller
             default => $books -> latest()
         };
 
-        $books = $books->get();
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = Cache::remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
