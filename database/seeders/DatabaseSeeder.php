821854a1d6f47bec23032fa1c61242c8c76cd5ea
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-07-22 23:58:22 +0200

    Star Rating component

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,21 +22,21 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        Book::factory(33)->create()->each(function($book){
+        Book::factory(44)->create()->each(function($book){
             $numReviews = random_int(5, 30);
 
             //factory method expects no arguments (or a single argument to customize the factory state), and bcs of that we need to use count
             Review::factory() -> count($numReviews) -> good() -> for($book) -> create();
         });
 
-        Book::factory(33)->create()->each(function($book){
+        Book::factory(44)->create()->each(function($book){
             $numReviews = random_int(5, 30);
 
             //factory method expects no arguments (or a single argument to customize the factory state), and bcs of that we need to use count
             Review::factory() -> count($numReviews) -> avg() -> for($book) -> create();
         });
 
-        Book::factory(34)->create()->each(function($book){
+        Book::factory(44)->create()->each(function($book){
             $numReviews = random_int(5, 30);
 
             //factory method expects no arguments (or a single argument to customize the factory state), and bcs of that we need to use count
--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -36,7 +36,7 @@
             </div>
             <div>
                 <div class="book-rating">
-                    {{number_format($book->reviews_avg_rating, 1)}}
+                    <x-star-rating :rating="number_format($book->reviews_avg_rating, 1)" />
                 </div>
                 <div class="book-review-count">
                 out of  {{$book->reviews_count}}
